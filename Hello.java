c0adf367524be4eb82f103c51992931a7e5644cb
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-11-15 11:49:48 +0530

    Manual Testing notes till Nov 15th

--- Hello.java
+++ Hello.java
@@ -1,12 +1,18 @@
+//this is a java program to print Hello java on console
+/* developed by riyaz
+   developed on 14th Nov
+   reviewed by Srinath */
 class Hello
 {
 
 public static void main(String[] args)
 {
 
+//this is to print some string on console
 System.out.println("Hello Java ...");
 
 }
 
 
 }
+//end of program
---
+++ Sum.java
@@ -0,0 +1,25 @@
+//import java.util.*;
+import java.util.Scanner;
+class Sum
+{
+
+public static void main(String[] args)
+{
+
+System.out.println("Enter some value for x :");
+
+Scanner input1=new Scanner(System.in);
+int x=input1.nextInt();
+
+System.out.println("Enter some value for y :");
+
+Scanner input2=new Scanner(System.in);
+int y=input2.nextInt();
+
+System.out.println("Sum is "+(x+y));
+
+
+}
+
+
+}
