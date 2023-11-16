e7eefae65810e974b13b1d0370a18225680ea026
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-11-16 11:49:16 +0530

    Manual Testing notes till Nov 16th

---
+++ VariablesDemo1.java
@@ -0,0 +1,27 @@
+public class VariablesDemo1
+{
+
+static int a=40;
+int z;
+public static void main(String[] args)
+{
+
+int x=10;
+
+int y=0;
+
+System.out.println(x);
+
+System.out.println(y);
+
+VariablesDemo1 obj=new VariablesDemo1();
+
+System.out.println(obj.z);
+
+System.out.println(VariablesDemo1.a);
+
+}
+
+
+
+}
